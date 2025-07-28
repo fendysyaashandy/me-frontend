9ac4ca2421177d352712ac7eba49f8c5cedd24ce
fendysyaashandy <fendysyaashandy@gmail.com>
2025-07-28 17:03:29 +0700

    first setup

---
+++ app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import Dashboard from "./dashboard";
+// import Login from "./login";
+// import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Dashboard />;
+}
